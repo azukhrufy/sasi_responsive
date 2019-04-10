46cc51c933fc987b5755e07c0cb04adaed18cfbd
Azukhrufy <anandazukhrufawalwi@gmail.com>
2019-04-10 15:43:50 +0700

    v1

---
+++ index.php
@@ -0,0 +1,143 @@
+ <!-- Load an icon library to show a hamburger menu (bars) on small screens -->
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>SMAN 1 SINDANG</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="navbar.css" >
+<link rel="stylesheet" type="text/css" href="content.css" >
+<link rel="stylesheet" type="text/css" href="bootstrap.css" >
+ </head>
+  <body>
+  <script type="text/javascript">
+  	/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+	function myFunction() {
+	  var x = document.getElementById("myTopnav");
+	  if (x.className === "topnav") {
+	    x.className += " responsive";
+	  } else {
+	    x.className = "topnav";
+	  }
+	} 
+  </script>
+
+<div class="topnav" id="myTopnav">
+  <a href="#home"><img src="icon.png"></a>
+  <a href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()"><img src="menu.png" width="50px" height="50px" style="float: right;"></a>
+</div> 
+
+<div class="content">
+<!-- <h1>SEKILAS TENTANG SMA NEGERI 1 SINDANG</h1> -->
+    <img src="ban.png"  class="banner" style="max-width: 100%; max-height: 450px;">
+  <div class="rowcontent">
+  <br>
+    <h2 class="title">Selamat Datang Di SMAN 1 Sindang</h2>
+    <div class="break">
+          <img src="break.png">
+        </div>
+        <div class="isi"> 
+        <img src="sasi.png">
+        <br>
+          <p>
+            SMA Negeri 1 Sindang adalah Sekolah Menengah Atas Negeri yang berada di Kota Indramayu, Kabupaten Indramayu. Sekolah ini beralamat di Jl. Letjend. M.T. Haryono, Sindang – Indramayu. Dengan motto <b><i>"The House Of Champions"</i></b>Bersama menciptakan asa dan harapan mencerdaskan anak bangsa menjadikan kehidupan yang lebih baik dengan semangat para JUARA.
+          </p>
+          <br>
+        <div class=" col-lg-12 isi">
+            <div class="row">
+                <div class="col-lg-4">
+                      <img src="books.png">
+                      <br>
+                      <h4 class="title">Menunjang E - Learning</h4>
+                      <div class="break">
+                       <img src="break.png">
+                      </div>
+                      <p class="isi">
+                          Penerapan Teknologi Informasi Untuk Pengelolan Segala Bidang Sekolah, dengan penerapan teknologi SMA Negeri 1 Sindang siap menghadapi tantangan era digitalisasi
+                      </p>
+                </div>
+          
+                <div class="col-lg-4">
+                  <img src="trophy.png">
+                  <br>
+                  <h4 class="title">House Of Champions</h4>
+                  <div class="break">
+                       <img src="break.png">
+                  </div>
+                 
+                  <p class="isi">
+                      Slogan The House of Champions bukan tanpa sebab SMA Negeri 1 Sindang telah mencetak beberapa medali juara baik dibidang akademik maupun non akademik
+                  </p>
+                </div>
+          
+              <div class="col-lg-4">
+                <img src="hat.svg">
+                <br>
+                <h4 class="title">Atmosfir Daya Saing</h4>
+                <div class="break">
+                       <img src="break.png">
+                  </div>
+                <p class="isi">
+                    Dengan menanamkan rasa kompetisi dan bersaing secara sehat diharapkan para peserta didik berusaha sebaik mungkin untuk menjadi yang terbaik dari yang terbaik
+                </p>
+              </div>
+            </div>
+        </div>
+        <br><br><br>
+        <h2 class="title">Galeri SMAN 1 SINDANG</h2>
+        <div class="break">
+          <img src="break.png">
+        </div>
+        <br>
+        <div class="col-lg-12">
+         <div class="scrollmenu">
+          <img src="img.jpg">
+          <img src="imgg.jpg">
+          <img src="imggg.jpg">
+          <img src="imgggg.jpg">
+          <img src="img.jpg">
+          <img src="imgg.jpg">
+          <img src="imggg.jpg">
+          <img src="imgggg.jpg">
+        </div>
+        <br><br><br>
+        <h2 class="title">Sambutan Kepala Sekolah</h2>
+        <div class="break">
+          <img src="break.png">
+        </div>
+        
+          <div class="col-lg-8-modif">
+            <div class="row">
+              <div class="col-lg-3">
+                <div class="avatar">
+                 <img src="kepsek.jpg" alt="avatar">
+                </div>
+              </div>
+
+              <div class="col-lg-9-modif">
+              <br>
+                <p style="text-align: justify;">
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." <i>Dr.H.Ahmad.M.Pd</i>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+<!-- <p style="text-align: justify;">
+SMAN 1 Sindang Indramayu berdiri sejak tahun 1961, terletak di Jalan Letjen MT Haryono Kecamatan Sindang Kabupaten Indramayu.Pada awal berdiri namanya SMAN Indramayu, dan beberapa kali mengalami perubahan nomenklatur (nama) sekolah , yaitu SMAN 1 Indramayu, SMUN 1 Sindang, dan sekarang SMAN 1 Sindang Indramayu. </p>
+<p style="text-align: justify;">
+Pada tahun 2004 dan 2007, memperoleh Akreditasi A (amat baik) dari Badan Akreditasi Nasional Sekolah Pada tahun 2007, berdasarkan Keputusan Direktur Pembinaan SMA Direktorat Jenderal Manajemen Pendidikan Dasar dan Menengah Departemen Pendidikan Nasional nomor 697/C4/MN/2007 tanggal 18 Juli 2007, ditetapkan sebagai Rintisan Sekolah Bertaraf Internasional (RSBI). Pada tahun 2009 berhasil meraih Serifikat ISO 9001 : 2008 dari Badan Sertifikasi Internasional. 
+</p> -->
+
+<p style="text-align: justify;">
+  
+</p>
+</div>
+ </body>
+  </html>
